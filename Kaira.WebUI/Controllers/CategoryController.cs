0cbe5bebab856cc701051577c738bc5e518768c3
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-12-11 22:28:58 +0300

    Add project files.

---
+++ Kaira.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Kaira.WebUI.Repositories.CategoryRepositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Kaira.WebUI.Controllers
+{
+    public class CategoryController(ICategoryRepository _categoryRepository) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return View(categories);
+        }
+    }
+}
---
+++ Kaira.WebUI/Repositories/CategoryRepositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using Kaira.WebUI.Context;
+using Kaira.WebUI.Dtos.CategoryDtos;
+using System.Data;
+
+namespace Kaira.WebUI.Repositories.CategoryRepositories
+{
+    public class CategoryRepository(AppDbContext _context) : ICategoryRepository
+    {
+        private readonly IDbConnection _db = _context.CreateConnection();
+
+
+        public async Task CreateAsync(CreateCategoryDto categoryDto)
+        {
+            string query = "INSERT INTO Categories (Name) VALUES (@Name)";
+            var parameters = new DynamicParameters(categoryDto);
+            await _db.ExecuteAsync(query, parameters);
+
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            string query ="DELETE FROM Categories WHERE CategoryId=@CategoryId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@CategoryId", id);
+           await _db.ExecuteAsync(query, parameters);
+        }
+
+        public async Task<IEnumerable<ResultCategoryDto>> GetAllAsync()
+        {
+            string query = "SELECT * FROM Categories";
+            return await _db.QueryAsync<ResultCategoryDto>(query);
+        }
+
+        public async Task<UpdateCategoryDto> GetByIdAsync(int id)
+        {
+           string query = "SELECT * FROM Categories WHERE CategoryId=@CategoryId";
+            var parameters = new DynamicParameters();
+            parameters.Add("@CategoryId", id);
+            return await _db.QueryFirstOrDefaultAsync<UpdateCategoryDto>(query, parameters);
+        }
+
+        public async Task UpdateAsync(UpdateCategoryDto categoryDto)
+        {
+            string query = "UPDATE Categories SET Name=@Name WHERE CategoryId=@CategoryId";
+            var parameters = new DynamicParameters(categoryDto);
+            await _db.ExecuteAsync(query, parameters);
+        }
+    }
+}
