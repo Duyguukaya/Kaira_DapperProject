2e89145b9b1bfee43e9d94d758cf7ca86c950421
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-12-13 13:08:06 +0300

    anasayfa componentlara ayrıldı.

---
+++ Kaira.WebUI/ViewComponents/_FeatureComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kaira.WebUI.ViewComponents
+{
+    public class _FeatureComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kaira.WebUI/ViewComponents/_ScriptComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kaira.WebUI.ViewComponents
+{
+    public class _ScriptComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
